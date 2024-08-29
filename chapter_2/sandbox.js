c6c1ff4951657dc2375281218292f1a5f6b9a624
csumaoang <csumaoang@academic.rrc.ca>
2024-08-29 11:24:19 -0500

    Added testing to sandbox.js

--- chapter_2/sandbox.js
+++ chapter_2/sandbox.js
@@ -1,1 +1,20 @@
 alert('hello, world');
+
+console.log(1);
+console.log(2);
+
+// log things to console
+let age = 25;
+let year = 2019;
+console.log(age, year);
+age = 30;
+console.log(age);
+
+// cannot overwrite constants
+const points = 100;
+// points = 50;
+console.log(points); 
+
+// old version of variable declare
+var score = 75;
+console.log(score);
